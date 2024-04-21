ce406afe1a03c76c0c048ac60c900cb91733e0fe
Julian Prieto <prietojulian2003@gmail.com>
2024-04-20 21:07:41 -0300

    add colors to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fffff1",
+        secondary: "#b3b3b3",
+        green: "#1ed45e",
+      },
+    },
+  },
+  plugins: [],
+};
